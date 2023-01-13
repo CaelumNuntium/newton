6366e6e624fcb02596ac7d114bfbfab53c22401b
CaelumNuntium <110852638+CaelumNuntium@users.noreply.github.com>
2023-01-13 22:07:58 +0300

    Update newton.h

--- newton.h
+++ newton.h
@@ -2,5 +2,3 @@ void newton(int, double* (*f)(double*), int, double*, double*);
 void gauss(int, int, double*, double*, double*);
 void diff(double* (*f)(double*), double*, int, double*);
 double norm(int, double*);
-//double* (*vectorize(double (*f)(double)))(double*);
-
